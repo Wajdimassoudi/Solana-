cc1fde474b72746b3c1dafca35f84e1bc5222a11
Wajdimassoudi <45901724+Wajdimassoudi@users.noreply.github.com>
2026-01-27 15:13:26 +0100

    Delete constants.ts

--- constants.ts
+++
@@ -1,5 +0,0 @@
-// This is a fictional address for UI demonstration purposes.
-export const DEPOSIT_ADDRESS = 'SoLw1nABCDEfGHiJKLmnOPqrstuvWXYZ123456789';
-
-// This is the final destination address provided in the user request.
-export const FINAL_DESTINATION_ADDRESS = '9S4B9Ph3mQ7RaEGNSdCyWyYMYh8a9mZdbHgZDYiqqtfg';
